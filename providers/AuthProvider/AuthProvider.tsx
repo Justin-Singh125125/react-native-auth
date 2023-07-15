55587fdbdc1982b0f7751fe1de6cda7309dca9bf
Justin Singh <jsingh@rcglogistics.com>
2023-07-15 10:46:47 -0700

    storing the refresh token so we can automatically log the user in

--- providers/AuthProvider/AuthProvider.tsx
+++ providers/AuthProvider/AuthProvider.tsx
@@ -1,7 +1,8 @@
-import { isValidElement } from 'react';
+import * as SecureStore from 'expo-secure-store';
 import { useAutoDiscovery } from 'expo-auth-session';
 import { AuthContext, AuthContextProps } from '../../context';
 import { AuthProviderProps } from './AuthProvider.types';
+import { REFRESH_TOKEN_CACHE_KEY } from './AuthProvider.constants';
 import { useMemo, useState } from 'react';
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
@@ -12,7 +13,13 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [tokenResponse, setTokenResponse] = useState<AuthContextProps['tokenResponse']>(null);
 
   const signin: AuthContextProps['signin'] = async (tokenResponse) => {
+    if (!tokenResponse.refreshToken) {
+      throw new Error('No refresh token was provided.');
+    }
+
     setTokenResponse(tokenResponse);
+
+    await SecureStore.setItemAsync(REFRESH_TOKEN_CACHE_KEY, tokenResponse.refreshToken);
   };
 
   const value = useMemo<AuthContextProps>(() => {
