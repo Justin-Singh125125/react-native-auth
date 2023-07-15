390add3e10f376bd504349b38dd045c40ed0c4eb
Justin Singh <jsingh@rcglogistics.com>
2023-07-15 01:24:07 -0700

    setup discovery

--- providers/AuthProvider/AuthProvider.tsx
+++ providers/AuthProvider/AuthProvider.tsx
@@ -1,14 +1,18 @@
-import { AuthContext } from '../../context';
+import { useAutoDiscovery } from 'expo-auth-session';
+import { AuthContext, AuthContextProps } from '../../context';
 import { AuthProviderProps } from './AuthProvider.types';
+import { useMemo } from 'react';
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  return (
-    <AuthContext.Provider
-      value={{
-        discovery: null,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+  const discovery = useAutoDiscovery(
+    'https://login.microsoftonline.com/a4310d02-5ebc-4101-8b85-ab8a740dd294/v2.0'
   );
+
+  const value = useMemo<AuthContextProps>(() => {
+    return {
+      discovery,
+    };
+  }, [discovery]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
