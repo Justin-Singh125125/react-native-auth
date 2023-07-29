b8ebd1ed0f5f84cc0400f61efd3e3446c827c0c7
Justin Singh <jsingh@rcglogistics.com>
2023-07-29 10:54:10 -0700

    reduced hook logic

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import { GetItemsResponse } from './api.types';
+import { TokenResponse } from 'expo-auth-session';
 
 class Api {
   instance: AxiosInstance;
@@ -8,6 +9,17 @@ class Api {
     this.instance = axios.create({
       baseURL: 'http://localhost:8000',
     });
+  }
+
+  setDefaultAccessToken(accessToken: TokenResponse['accessToken']) {
+    this.instance.defaults.headers.common['Authorization'] = accessToken;
+  }
+
+  setConfigAccessToken(
+    config: InternalAxiosRequestConfig,
+    accessToken: TokenResponse['accessToken']
+  ) {
+    config.headers['Authorization'] = accessToken;
   }
 
   getItems() {
--- providers/AuthProvider/hooks/useApiInterceptor/useApiInterceptor.ts
+++ providers/AuthProvider/hooks/useApiInterceptor/useApiInterceptor.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
+import { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { api } from '../../../../api';
 import { UseApiInterceptorProps } from './useApiInterceptor.types';
-import { AxiosError } from 'axios';
 
 export const useApiInterceptor = ({
   tokenResponse,
@@ -9,51 +9,48 @@ export const useApiInterceptor = ({
   refreshTokenConcurrently,
 }: UseApiInterceptorProps) => {
   useEffect(() => {
-    api.instance.defaults.headers['Authorization'] = tokenResponse?.accessToken;
+    if (tokenResponse) {
+      api.setDefaultAccessToken(tokenResponse.accessToken);
 
-    const requestInterceptor = api.instance.interceptors.request.use((config) => {
-      config.headers['Authorization'] = tokenResponse?.accessToken;
-      return config;
-    });
+      const requestInterceptor = api.instance.interceptors.request.use((config) => {
+        api.setConfigAccessToken(config, tokenResponse.accessToken);
+        return config;
+      });
 
-    const responseInterceptor = api.instance.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      async (error: AxiosError) => {
-        const originalRequest = error.config;
-        console.log('hello');
+      const responseInterceptor = api.instance.interceptors.response.use(
+        (response) => {
+          return response;
+        },
+        async (error: AxiosError) => {
+          const originalRequestConfig = error.config;
 
-        console.log(!originalRequest?._retry);
+          if (
+            originalRequestConfig &&
+            error.response?.status === 401 &&
+            !originalRequestConfig._retry
+          ) {
+            originalRequestConfig._retry = true;
 
-        if (originalRequest && error.response?.status === 401 && !originalRequest?._retry) {
-          originalRequest._retry = true;
+            try {
+              const { accessToken } = await refreshTokenConcurrently(tokenResponse);
 
-          try {
-            if (!tokenResponse) {
-              throw new Error('');
+              api.setConfigAccessToken(originalRequestConfig, accessToken);
+
+              return api.instance(originalRequestConfig);
+            } catch (refreshError) {
+              await signout();
+              return Promise.reject(refreshError);
             }
-
-            const { accessToken, tokenType } = await refreshTokenConcurrently(tokenResponse);
-
-            console.log(accessToken);
-
-            originalRequest.headers['Authorization'] = accessToken;
-
-            return api.instance(originalRequest);
-          } catch (refreshError) {
-            signout();
-            return Promise.reject(refreshError);
           }
+
+          return Promise.reject(error);
         }
+      );
 
-        return Promise.reject(error);
-      }
-    );
-
-    return () => {
-      api.instance.interceptors.request.eject(requestInterceptor);
-      api.instance.interceptors.response.eject(responseInterceptor);
-    };
+      return () => {
+        api.instance.interceptors.request.eject(requestInterceptor);
+        api.instance.interceptors.response.eject(responseInterceptor);
+      };
+    }
   }, [tokenResponse, signout]);
 };
